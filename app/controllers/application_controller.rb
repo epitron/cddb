57cdd665d3759392bac02398abd1b0c106448cff
Chris Gahan <chris@ill-logic.com>
2009-09-23 20:44:21 -0400

    Sessions suck!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  #protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  session :off
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
