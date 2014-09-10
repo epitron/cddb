0df136107a63c928e402d069352c02ea182abe4d
epitron <chris@ill-logic.com>
2014-09-10 16:47:30 -0400

    Ruby 2.1.2 fixes

--- Gemfile
+++ Gemfile
@@ -1,9 +1,11 @@
 source 'http://rubygems.org'
 
-gem 'rails', '~> 2.3.17'
+gem 'rails', '~> 2.3.18'
 gem 'haml'
 gem 'sass'
 gem 'mysql', '~> 2.9.1'
+gem 'iconv'
+gem 'yard'
 
 group :development, :test do
   gem 'test-unit', '1.2.3'
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Be sure to restart your server when you modify this file
 
+require 'yard/rubygems/backports/gem'
+require 'yard/rubygems/backports/source_index'
+
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 
